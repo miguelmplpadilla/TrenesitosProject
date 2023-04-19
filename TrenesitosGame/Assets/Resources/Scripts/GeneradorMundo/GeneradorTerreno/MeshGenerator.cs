b601350eb72c07c51785eb4b1c33dd539cc06419
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2023-04-19 09:51:07 +0200

    Generación terreno aleatorio

--- TrenesitosGame/Assets/Resources/Scripts/GeneradorMundo/GeneradorTerreno/MeshGenerator.cs
+++ TrenesitosGame/Assets/Resources/Scripts/GeneradorMundo/GeneradorTerreno/MeshGenerator.cs
@@ -1,40 +1,90 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MeshGenerator : MonoBehaviour
 {
 
     private Mesh mesh;
 
-    private Vector3[] vertices;
-    private int[] triangles;
+    [SerializeField] private Vector3[] vertices;
+    [SerializeField] private int[] triangles;
 
     public int xSize = 20;
     public int zSize = 20;
+
+    public int numVerticesPlanos = 2;
+    public float offsetPerlinNoise = 0;
     
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        
-        createShape();
+
+        createShape(-1);
         updateMesh();
+        
+        GetComponent<MeshCollider>().sharedMesh = mesh;
     }
 
-    void createShape()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void createShape(float posNeg)
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-
-        int i = 0;
-
-        for (int z = 0; z <= zSize; z++)
+        
+        int cont = 0;
+        
+        for (int z = 0; z < numVerticesPlanos; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                vertices[cont] = new Vector3(x, 0, z);
+                cont++;
+            }
+        }
+        
+        for (int i = cont, z = numVerticesPlanos; z <= zSize; z++)
+        {
+            offsetPerlinNoise += 0.2f;
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * offsetPerlinNoise, z * offsetPerlinNoise) * 2;
+                vertices[i] = new Vector3(x, y, z);
                 i++;
             }
+        }
+        
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+        
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+
+            vert++;
         }
     }
 
@@ -46,5 +96,16 @@ public class MeshGenerator : MonoBehaviour
         mesh.triangles = triangles;
         
         mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices != null)
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Gizmos.DrawSphere(vertices[i], .1f);
+            }
+        }
     }
 }
